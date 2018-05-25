b35c7aef391a3739151e20e426ead557534d292c
Anubhav Dhawan <anubhav756@gmail.com>
2018-05-25 16:45:38 +0530

    Bind tab to routing

--- client/src/scenes/Home/Home.js
+++ client/src/scenes/Home/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import _ from 'lodash';
 import SwipeableViews from 'react-swipeable-views';
 import {
   AppBar,
@@ -12,6 +15,11 @@ import {
 import Profile from './scenes/Profile';
 import TabContainer from './components/TabContainer';
 
+const routeToTabMap = [
+  '/profile',
+  '/location',
+];
+
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -21,23 +29,48 @@ class Home extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleChangeIndex = this.handleChangeIndex.bind(this);
   }
-  handleChange(event, value) {
-    this.setState({ value });
+  componentWillMount() {
+    const {
+      location: {
+        pathname,
+      },
+    } = this.props;
+
+    this.setState({ value: _.indexOf(routeToTabMap, pathname) });
   }
-  handleChangeIndex(value) {
+  componentWillReceiveProps(nextProps) {
+    const {
+      location: {
+        pathname,
+      },
+    } = nextProps;
+
+    this.setState({ value: _.indexOf(routeToTabMap, pathname) });
+  }
+  handleChange(value) {
+    const {
+      history: {
+        push,
+      },
+    } = this.props;
+
+    push(routeToTabMap[value]);
     this.setState({ value });
   }
   render() {
     const { value } = this.state;
+
+    if (value === -1) {
+      return <Redirect to={routeToTabMap[0]} />;
+    }
 
     return (
       <div>
         <AppBar position="static">
           <Tabs
             value={value}
-            onChange={this.handleChange}
+            onChange={(event, newValue) => this.handleChange(newValue)}
             fullWidth
             centered
           >
@@ -47,7 +80,7 @@ class Home extends Component {
         </AppBar>
         <SwipeableViews
           index={value}
-          onChangeIndex={this.handleChangeIndex}
+          onChangeIndex={this.handleChange}
         >
           <TabContainer><Profile /></TabContainer>
           <TabContainer>Coming soon...</TabContainer>
@@ -56,5 +89,14 @@ class Home extends Component {
     );
   }
 }
+
+Home.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Home;
