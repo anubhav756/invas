fb28bf11dcff634efaf8181c6c9e5c5af3996fb8
Anubhav Dhawan <anubhav756@gmail.com>
2018-05-25 15:03:50 +0530

    Connect redux and react-router

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { CssBaseline } from '@material-ui/core';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import rootReducer from '../../reducers';
+
 import Home from '../../screens/Home';
+
+const store = createStore(rootReducer);
 
 const App = () => (
   <div>
     <CssBaseline />
-    <Home />
+    <Provider store={store}>
+      <Router>
+        <Route path="/" component={Home} />
+      </Router>
+    </Provider>
   </div>
 );
 
